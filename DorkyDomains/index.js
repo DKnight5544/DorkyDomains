1f741b16bdef5e94e0a2aec7d1eda033ab1cd4c2
David Knight <dave@mostlyspam>
2024-06-12 09:52:47 -0700

    Correct Spelling and attempt to dynamically size the text in the domains listing.

--- DorkyDomains/index.js
+++ DorkyDomains/index.js
@@ -7,14 +7,16 @@ let prevRand = 0;
 function begin() {
     domains = document.querySelectorAll('.domain');
     let max = Math.floor(domains.length - 1);
-    setInterval(cycleDivs, 1500);
+    setInterval(colorizeRandomly, 1500);
+    setInterval(setFontSizes, 1500);
 }
-function cycleDivs() {
+function colorizeRandomly() {
     domains[prevRand].style.backgroundColor = "#4f84bd";
-    let rnd = getRandomNumber(0, domains.length);
+    let rnd = getRandomNumber(0, domains.length - 1);
     domains[rnd].style.backgroundColor = "red";
-    prevRand = rnd;
+    prevRand = rnd;    
 };
+
 
 function getRandomNumber(min, max) {
     do {
@@ -22,3 +24,40 @@ function getRandomNumber(min, max) {
     } while(rnd === prevRand)
     return rnd;
 }
+
+function setFontSizes(){
+    for (let i = 0; i < domains.length; i++) {
+        setFontSize(domains[i]);
+    }
+}
+
+
+function setFontSize(container) {
+    // Create a temporary span element to measure text size
+    let tempDiv = document.createElement('div');
+    tempDiv.innerHTML = container.innerHTML; // Use the innerHTML of the container
+    tempDiv.className = container.className; // Adopt the class of the container
+
+    // Initialize variables for binary search
+    let minFontSize = 1; // Minimum font size
+    let maxFontSize = 25; // Maximum font size, can be adjusted
+    let currentFontSize;
+
+    // Binary search to find the maximum font size
+    while (minFontSize < maxFontSize - 1) {
+        currentFontSize = (minFontSize + maxFontSize) / 2;
+        tempDiv.style.fontSize = `${currentFontSize}px`;
+
+        // Check if the current font size is too large
+        if (tempDiv.scrollWidth > container.clientWidth || tempDiv.scrollHeight > container.clientHeight) {
+            maxFontSize = currentFontSize - 1;
+        } else {
+            minFontSize = currentFontSize + 1;
+        }
+    }
+
+    container.style.fontSize = tempDiv.style.fontSize;
+
+
+}
+
